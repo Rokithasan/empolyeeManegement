1b63bb49b8a7d537d4100f5c7af48bb4437a7618
Rokit Hasan <lotifnazma@gmail.com>
2022-06-24 15:28:08 +0600

    delete unused item

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.empolyeemanegement {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.empolyeemanegement to javafx.fxml;
-    exports com.example.empolyeemanegement;
-}
